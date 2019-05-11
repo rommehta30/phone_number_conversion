eade8ef17b3712f85a7a2afebaca45f2b02ab0ec
Romil Mehta <rommehta30@gmail.com>
2019-05-11 17:46:53 +0530

    reading dictionary file and arranging words based on length

--- number_conversion.rb
+++ number_conversion.rb
@@ -14,5 +14,32 @@ class NumberConversion
     return "Length should be equal to 10" if ph_no_array.length != 10
     # 2. 0 or 1 Presence check
     return "Should not contain 1 or 0" if ph_no_array.select { |a| a == '1' or a == '0' }.length > 0
+    
+    # Init phone letters
+    phone_letters = {
+      "2" => ['a', 'b', 'c'],
+      "3" => ['d', 'e', 'f'],
+      "4" => ['g', 'h', 'i'],
+      "5" => ['j', 'k', 'l'],
+      "6" => ['m', 'n', 'o'],
+      "7" => ['p', 'q', 'r', 's'],
+      "8" => ['t', 'u', 'v'],
+      "9" => ['w', 'x', 'y', 'z']
+    }
+
+    # Getting phone no character array using phone letters
+    ph_no_char_array = ph_no_array.map { |n| phone_letters[n] }
+
+    # Initlaizing dictionary words
+    dictionary_words = {}
+    (1..10).each do |i|
+      dictionary_words[i] = []
+    end
+    
+    # Reading dictionary file and setting dictionary words and arranging words based on length
+    File.readlines('dictionary.txt').each do |word|
+      word = word.strip()
+      dictionary_words[word.length].push(word.downcase) if word.length > 2 && word.length <= 10
+    end
   end
 end
